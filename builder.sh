3edca8290b3585abc143c6c3b8df03b0b8dd10d0
J. Bruni <contato@jbruni.com.br>
2014-05-23 14:25:11 -0300

    Now I am finally starting to understand something

--- builder.sh
+++ builder.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 bower install
-cat bower_components/famous-angular/dist/famous-angular.js splendsome.inc > meteor-famous-angular.js
+cat bower_components/famous-angular/dist/famous-angular.js splendsome.inc > index.js
