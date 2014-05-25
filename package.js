fe51c3de2bc2d76958506eda5d10070d3312775b
J. Bruni <contato@jbruni.com.br>
2014-05-24 23:47:32 -0300

    Added, removed and connected some dots

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Seamless Famous animations in any device and super-heroic AngularJS front-end MVW framework in your Meteor realtime distributed app"
+  summary: "Seamless Famo.us animations in any device and super-heroic AngularJS front-end MVW framework in your Meteor realtime distributed app"
 });
 
 Package.on_use(function(api) {
